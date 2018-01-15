510467401cc861be1233f0ae8458580eb11674a9
paula.bass.werner <basswerner.p@gmail.com>
2018-01-15 16:09:05 -0500

    Use Glide and ButterKnife libraries in app

--- app/src/main/java/io/intrepid/thirdpartylibraryexcercise/MainActivity.java
+++ app/src/main/java/io/intrepid/thirdpartylibraryexcercise/MainActivity.java
@@ -1,18 +1,26 @@
 package io.intrepid.thirdpartylibraryexcercise;
 
 import android.app.ProgressDialog;
-import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity implements MainContract.View, DownloadCatImageTask.Callback {
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity implements MainContract.View {
+
+    @BindView(R.id.image_view)
     ImageView imageView;
-    Button getNewCatButton;
 
     private MainPresenter presenter;
     private ProgressDialog dialog;
@@ -23,15 +31,8 @@ public class MainActivity extends AppCompatActivity implements MainContract.View
         setContentView(R.layout.activity_main);
         presenter = new MainPresenter();
         presenter.bindView(this);
+        ButterKnife.bind(this);
 
-        imageView = (ImageView) findViewById(R.id.image_view);
-        getNewCatButton = (Button) findViewById(R.id.new_cat_button);
-        getNewCatButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                onNewCatButtonClick();
-            }
-        });
     }
 
     @Override
@@ -40,6 +41,7 @@ public class MainActivity extends AppCompatActivity implements MainContract.View
         presenter.bindView(this);
     }
 
+    @OnClick(R.id.new_cat_button)
     public void onNewCatButtonClick() {
         presenter.onNewCatButtonClick();
     }
@@ -62,20 +64,55 @@ public class MainActivity extends AppCompatActivity implements MainContract.View
 
     @Override
     public void loadImageFromUrl(String url) {
-        DownloadCatImageTask downloadCatImageTask = new DownloadCatImageTask();
-        downloadCatImageTask.setCallback(this);
-        downloadCatImageTask.execute(url);
+        Glide.with(this)
+                .load(url)
+                .listener(new RequestListener<Drawable>() {
+                    @Override
+                    public boolean onLoadFailed(@Nullable GlideException e,
+                                                Object model,
+                                                Target<Drawable> target,
+                                                boolean isFirstResource) {
+                        dismissLoadingIndicator();
+                        return false;
+                    }
+
+                    @Override
+                    public boolean onResourceReady(Drawable resource,
+                                                   Object model,
+                                                   Target<Drawable> target,
+                                                   DataSource dataSource,
+                                                   boolean isFirstResource) {
+                        dismissLoadingIndicator();
+                        return false;
+                    }
+                })
+                .into(imageView);
     }
 
-    @Override
-    public void onImageResult(Bitmap bitmap) {
-        dismissLoadingIndicator();
-        imageView.setImageBitmap(bitmap);
-    }
 
     @Override
     protected void onStop() {
         super.onStop();
         presenter.unbindView();
     }
+
+//    public class LoggingListener<T,  implements RequestListener<R> {
+//
+//        @Override
+//        public boolean onLoadFailed(@Nullable GlideException e,
+//                                    Object model,
+//                                    Target<T> target,
+//                                    boolean isFirstResource) {
+//            return false;
+//        }
+//
+//        @Override
+//        public boolean onResourceReady(T resource,
+//                                       Object model,
+//                                       Target<T> target,
+//                                       DataSource dataSource,
+//                                       boolean isFirstResource) {
+//            return false;
+//        }
+//    }
 }
